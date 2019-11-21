f1f7620ba11971786ec8c2b8022e63911ca367c6
gbalejandro <alejandrobright@gmail.com>
2019-11-21 14:33:03 -0500

    Primer commit del proyecto.

---
+++ Sistem_Ventas/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Sistem_Ventas.Models;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Sistem_Ventas.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController(IServiceProvider serviceProvider)
+        {
+            CreateRoles(serviceProvider);
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private async Task CreateRoles(IServiceProvider serviceProvider)
+        {
+            String mensaje;
+            try
+            {
+                var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+                var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+                String[] rolesName = { "Admin", "User" };
+                foreach (var item in rolesName)
+                {
+                    var roleExist = await roleManager.RoleExistsAsync(item);
+                    if (!roleExist)
+                    {
+                        await roleManager.CreateAsync(new IdentityRole(item));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                mensaje = ex.Message;
+            }
+        }
+    }
+}
